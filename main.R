815af88d703de24b94e703d1df2399347c22ce3b
msaww <113406050+msaww@users.noreply.github.com>
2022-09-12 23:20:05 -0400

    Update main.R

---
+++ main.R
@@ -0,0 +1,10 @@
+#Provide a code that: 
+
+#Imports your data 
+
+#Tidy your data (including data transformations) 
+
+#Conduct a simple Exploratory Data Analysis 
+
+#Provide Descriptive Analytics 
+
