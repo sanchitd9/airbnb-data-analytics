a400f1167d5cdce63b1e7f8ecb2e148b2916fe2c
Sanchit Dass <sanchitd9@gmail.com>
2022-09-13 17:28:47 -0400

    Basic analysis done

--- main.R
+++ main.R
@@ -1,4 +1,4 @@
-library("tibble")
+library("tidyverse")
 
 # ------------------------------------------------------ #
 # IMPORT THE DATA
@@ -28,7 +28,8 @@ useful_vars <- c(
   "Beds",
   "Price",
   "Number.of.Reviews",
-  "Review.Scores.Rating"
+  "Review.Scores.Rating",
+  "Property.Type"
 )
 
 # Remove all the other variables from the data frame.
@@ -40,6 +41,7 @@ airbnb_df <- na.omit(airbnb_df)
 airbnb_df <- airbnb_df[airbnb_df$City != "", ]
 airbnb_df <- airbnb_df[airbnb_df$State != "", ]
 airbnb_df <- airbnb_df[airbnb_df$Country != "", ]
+airbnb_df <- airbnb_df[airbnb_df$Property.Type != "", ]
 
 # Check the data again.
 glimpse(airbnb_df)
@@ -52,5 +54,31 @@ airbnb_df <- airbnb_df[airbnb_df$Price > 0, ]
 
 
 # ------------------------------------------------------ #
-# EXPLORATORY ANALYSIS
+# INITIAL EXPLORATORY AND DESCRIPTIVE ANALYSIS FOR USA
 # ------------------------------------------------------ #
+
+usa_listings = airbnb_df[airbnb_df$Country == "United States", ]
+
+# Histogram of the price.
+ggplot(data = usa_listings) + geom_histogram(mapping = aes(x = usa_listings$Price), bins = 50, col = "#000000", fill = "#CCFFCC") + labs(x = "Price (per night)", y = "Count") + scale_x_continuous(breaks = c(seq(0, max(usa_listings$Price), 100)))
+
+# Box plot of price for each property type.
+ggplot(data = usa_listings) + geom_boxplot(mapping = aes(x = usa_listings$Price, y = usa_listings$Property.Type)) + labs(x = "Price (per night)", y = "Property Type")
+
+# Scatter plot of price vs No. of Bedrooms
+ggplot(data = usa_listings) + geom_point(mapping = aes(x = usa_listings$Price, y = usa_listings$Bedrooms, color = usa_listings$Property.Type)) + labs(x = "Price (per night)", y = "No. of Bedrooms") + scale_y_continuous(breaks = c(0, max(usa_listings$Bedrooms), 1))
+
+# Scatter plot of price vs No. of Bathrooms
+ggplot(data = usa_listings) + geom_point(mapping = aes(x = usa_listings$Price, y = usa_listings$Bathrooms, color = usa_listings$Property.Type)) + labs(x = "Price (per night)", y = "No. of Bathrooms") + scale_y_continuous(breaks = c(0, max(usa_listings$Bathrooms), 1))
+
+# Scatter plot of price vs No. of Beds
+ggplot(data = usa_listings) + geom_point(mapping = aes(x = usa_listings$Price, y = usa_listings$Beds, color = usa_listings$Property.Type)) + labs(x = "Price (per night)", y = "No. of Beds") + scale_y_continuous(breaks = c(0, max(airbnb_df$Beds), 1))
+
+# Scatter plot of price vs Ratings
+ggplot(data = usa_listings) + geom_point(mapping = aes(x = usa_listings$Price, y = usa_listings$Review.Scores.Rating)) + labs(x = "Price (per night)", y = "Ratings (0 - 100)")
+
+# Scatter plot of ratings vs No. of ratings
+ggplot(data = usa_listings) + geom_point(mapping = aes(x = usa_listings$Review.Scores.Rating, y = usa_listings$Number.of.Reviews)) + labs(y = "No. of Reviews", x = "Ratings (0 - 100)")
+
+
+                                  
