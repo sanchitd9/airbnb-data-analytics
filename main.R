751a16b9f48627484cbfb36df2809b48bcb30f99
msaww <113406050+msaww@users.noreply.github.com>
2022-09-13 20:03:31 -0400

    Update main.R

--- main.R
+++ main.R
@@ -57,10 +57,21 @@ airbnb_df <- airbnb_df[airbnb_df$Price > 0, ]
 # INITIAL EXPLORATORY AND DESCRIPTIVE ANALYSIS FOR USA
 # ------------------------------------------------------ #
 
+# In order to understand the relationship between a rental's price and its 89 variables in the dataset, a descriptive analysis was carried out and the variables were classified into three main categories:   
+
+# 1. Property Information: including location, type etc.   
+
+# 2. Previous Reviews: including number of reviews, review scores etc.   
+
+# 3. Redundant Data: including listing url, name, notes, weekly price etc.  
+
+# The influential variables mainly consist of two parts: property information, and previous reviews. Subsequently, 11 representative variables were selected from those three categories of data to examine their correlation.
+
 usa_listings = airbnb_df[airbnb_df$Country == "United States", ]
 
 # Histogram of the price.
 ggplot(data = usa_listings) + geom_histogram(mapping = aes(x = usa_listings$Price), bins = 50, col = "#000000", fill = "#CCFFCC") + labs(x = "Price (per night)", y = "Count") + scale_x_continuous(breaks = c(seq(0, max(usa_listings$Price), 100)))
+# Most of the properties are priced around 50$ to 150$ range and the frequency is tapering as the price increases.
 
 # Box plot of price for each property type.
 ggplot(data = usa_listings) + geom_boxplot(mapping = aes(x = usa_listings$Price, y = usa_listings$Property.Type)) + labs(x = "Price (per night)", y = "Property Type")
@@ -80,5 +91,7 @@ ggplot(data = usa_listings) + geom_point(mapping = aes(x = usa_listings$Price, y
 # Scatter plot of ratings vs No. of ratings
 ggplot(data = usa_listings) + geom_point(mapping = aes(x = usa_listings$Review.Scores.Rating, y = usa_listings$Number.of.Reviews)) + labs(y = "No. of Reviews", x = "Ratings (0 - 100)")
 
-
+# The pattern in the change in the no. of bedrooms, bathrooms, beds do not significantly influence the prices.
+# Whereas the property type and location highly influence the prices.
+#
                                   
